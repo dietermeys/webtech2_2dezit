1fce784ae9450bb8d61af3c874ec1a6ee321eb5a
Dieter Meys <dietermeys@gmail.com>
2014-08-02 13:59:34 +0200

    werken met sockets

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	mongoose = require('mongoose');
 
 server.listen(3000);
+
+mongoose.connect('mongodb://localhost/scoreboard', function(err){
+	if(err){
+		console.log(err);
+		console.log('mislukt, maar waarom??');
+	}
+	else{
+		console.log('Connected');
+	}
+});
+
+var scoreboardSchema = mongoose.Schema({
+	id: Number,
+	min: String,
+	msg: String
+});
+
+var Update = mongoose.model('Update', scoreboardSchema);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/admin.html');
@@ -13,4 +32,30 @@ app.get('/scoreboard.html', function (req, res) {
 });
 app.get('/admin.html', function (req, res) {
   res.sendfile(__dirname + '/admin.html');
+});
+
+app.use(express.static(__dirname + '/public')); //css
+
+io.sockets.on('connection', function(socket){
+
+	var query = Update.find({});
+	query.sort('created').exec(function(err, docs){		//exec laat het werken
+		if(err) throw err;
+		socket.emit('load old updates', docs);
+	});
+
+	socket.on('send update', function(data){
+	//	var newMsg = new Update(data);//document aanmaken
+	//	newMsg.save(function(err){ //save
+	//		if(err) throw  err;
+	//	});
+		io.sockets.emit('new update', data);//iedereen kan het zien(ook ik)
+	});
+	socket.on('send time', function(data1){
+	//	var newMsg = new Update(data);//document aanmaken
+	//	newMsg.save(function(err){ //save
+	//		if(err) throw  err;
+	//	});
+		io.sockets.emit('new time', data1);//iedereen kan het zien(ook ik)
+	});
 });
