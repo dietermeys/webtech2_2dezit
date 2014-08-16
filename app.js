d5ba95f683d87fedb4033907c2c1cca08d70a7a9
Dieter Meys <dietermeys@gmail.com>
2014-08-16 13:26:14 +0200

    aanpassingen

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var express = require('express'),
 	io = require('socket.io').listen(server),
 	mongoose = require('mongoose');
 	minuten = [];
+	scoren = [];
 
 server.listen(3000);
 
-mongoose.connect('mongodb://localhost/scoreboard', function(err){
+mongoose.connect('mongodb://dieter:blackdown10@kahana.mongohq.com:10048/scoreboard', function(err){
 	if(err){
 		console.log(err);
 		console.log('mislukt, maar waarom??');
@@ -21,6 +22,7 @@ var scoreboardSchema = mongoose.Schema({
 	id: Number,
 	min: String,
 	msg: String,
+	score1: Number,
 	created: {type: Date, default: Date.now}
 });
 
@@ -41,7 +43,7 @@ app.use(express.static(__dirname + '/public')); //css
 io.sockets.on('connection', function(socket){
 
 	var query = Update.find({});
-	query.sort('-created').exec(function(err, docs){		//exec laat het werken
+	query.sort('created').exec(function(err, docs){		//exec laat het werken
 		if(err) throw err;
 		socket.emit('load old updates', docs);
 	});
@@ -50,7 +52,7 @@ io.sockets.on('connection', function(socket){
 		socket.minute = data;
 		minuten.push(socket.minute);
 		//updateMinutes();
-	
+
 	});
 /*
 	function updateMinutes(){
@@ -58,15 +60,16 @@ io.sockets.on('connection', function(socket){
 	}
 */
 	socket.on('send update', function(data){
-	var newMsg = new Update({msg: data, min: socket.minute});//document aanmaken
+	var newMsg = new Update({msg: data, min: socket.minute, score1: socket.score1});//document aanmaken
 		newMsg.save(function(err){ //save
 			if(err) throw  err;
 		});
-	io.sockets.emit('new update', {msg: data, min: socket.minute});//iedereen kan het zien(ook ik)
+	io.sockets.emit('new update', {msg: data, min: socket.minute, score1: socket.score1});//iedereen kan het zien(ook ik)
 	});
 
 	socket.on('click', function(data){
-        io.sockets.emit('scorehome+',data); 
+		socket.score1 = data;
+		scoren.push(socket.score1);
     });
 
     socket.on('click2', function(data){
