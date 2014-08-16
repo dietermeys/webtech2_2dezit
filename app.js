9f661804f29260c7fc70db615c59e610b4fafdba
Dieter Meys <dietermeys@gmail.com>
2014-08-16 17:00:27 +0200

    alles aangepast

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ var express = require('express'),
 	io = require('socket.io').listen(server),
 	mongoose = require('mongoose');
 	minuten = [];
-	scoren = [];
-
 server.listen(3000);
-
 mongoose.connect('mongodb://dieter:blackdown10@kahana.mongohq.com:10048/scoreboard', function(err){
 	if(err){
 		console.log(err);
@@ -22,7 +19,6 @@ var scoreboardSchema = mongoose.Schema({
 	id: Number,
 	min: String,
 	msg: String,
-	score1: Number,
 	created: {type: Date, default: Date.now}
 });
 
@@ -60,20 +56,51 @@ io.sockets.on('connection', function(socket){
 	}
 */
 	socket.on('send update', function(data){
-	var newMsg = new Update({msg: data, min: socket.minute, score1: socket.score1});//document aanmaken
+	var newMsg = new Update({msg: data, min: socket.minute});//document aanmaken
 		newMsg.save(function(err){ //save
 			if(err) throw  err;
 		});
-	io.sockets.emit('new update', {msg: data, min: socket.minute, score1: socket.score1});//iedereen kan het zien(ook ik)
+	io.sockets.emit('new update', {msg: data, min: socket.minute});//iedereen kan het zien(ook ik)
 	});
 
 	socket.on('click', function(data){
-		socket.score1 = data;
-		scoren.push(socket.score1);
+        io.sockets.emit('scorehome+',data);
     });
-
     socket.on('click2', function(data){
         io.sockets.emit('scorehome-',data); 
     });
+    socket.on('click3', function(data){
+        io.sockets.emit('scoreaway+',data);
+    });
+    socket.on('click4', function(data){
+        io.sockets.emit('scoreaway-',data); 
+    });
 
+	socket.on('click5', function(data){
+        io.sockets.emit('shothome+',data);
+    });
+    socket.on('click6', function(data){
+        io.sockets.emit('shothome-',data); 
+    });
+
+	socket.on('click7', function(data){
+        io.sockets.emit('foulhome+',data);
+    });
+    socket.on('click8', function(data){
+        io.sockets.emit('foulhome-',data); 
+    });
+
+	socket.on('click9', function(data){
+        io.sockets.emit('shotaway+',data);
+    });
+    socket.on('click10', function(data){
+        io.sockets.emit('shotaway-',data); 
+    });
+
+	socket.on('click11', function(data){
+        io.sockets.emit('foulaway+',data);
+    });
+    socket.on('click12', function(data){
+        io.sockets.emit('foulaway-',data); 
+    });
 });
